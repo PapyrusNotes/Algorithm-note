7264c61fd35571c6673833c0cf465dc2dd28d077
PapyrusNotes <chris170841@gmail.com>
2022-01-02 17:39:56 +0900

    add solution

--- DfsBfs/data-structure_test.py
+++ DfsBfs/data-structure_test.py
@@ -2,27 +2,41 @@ def deque_for_bfs(data: list):
     from collections import deque
 
 
+def recur_dfs(graph, v, visited):
+    # 방문처리
+    visited[v] = True
+    print(v, end=' ')
+
+    # v와 연결되어있는 노드들을 재귀적으로 호출
+    for i in graph[v]:
+        if not visited[i]:
+            recur_dfs(graph, i, visited)
+
+
 def stack_for_dfs(data: list):
     stack = []
 
 
-
-
 def main():
 
-    """
-    개행하면서 입력받기
-    total_element = int(input('total_element : '))
-    data_stream = [0]*total_element
-    for i in range(0, total_element):
-        data_stream[i] = int(input())
-    """
+    '''
+    그래프 연결정보 표현
+    0이 아닌 1번째 노드부터 존재한다고 가정.1에 2,3,8, 2에 1,7이, 3에 1,4,5, 4에 3,5, 5에 3,4, 6에 7, 7에 2,6,8, 8에 1,7연결
+    방문 여부 정보 초기화
+    처음에는 전부 방문하지 않았으므로 FALSE로 초기화
+   '''
 
-    # 공백 기준으로 한 줄에 입력받기
-    data_stream = list(map(int, input().split()))
+    graph = [[], [2, 3, 8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
+    visited = [False]*len(graph)
 
-    deque_for_bfs(data_stream)
-    stack_for_dfs(data_stream)
+    # 재귀로 dfs 구현
+    # recur_dfs(graph, 1, visited)  # graph, 시작 노드, 방문여부 정보를 매개변수로 넣는다.
+
+    # 스택으로 dfs 구현
+    # stack_for_dfs(data_stm)
+
+    # deque로 bfs 구현
+    # deque_for_bfs(data_stream)
 
 
 if __name__ == '__main__':
--- greedy/rescue_boat_ver2.py
+++ greedy/rescue_boat_ver2.py
@@ -1,14 +1,14 @@
 def solution(people, limit):
     answer = 0
     people.sort()
-    left = 0
+    left = 0                                # 정렬후 좌, 우 커서를 배치하였다.
     right = len(people) - 1
-    while left < right:
-        if people[left] + people[right] <= limit:
-            left += 1
-            right -= 1
+    while left < right:                     # left -> right, right-> left로 움직이면서 교차되는 순간 모든 사람이 탈출한다.
+        if people[left] + people[right] <= limit:   # 가장 작은 몸무게, 가장 무거운 몸무게가 한계값을 넘지 않으면
+            left += 1                               # 가장 작은 몸무게 인원을 태운다.
+            right -= 1                              # 가장 무거운 몸무게 인원을 태운다.
         else:
-            right -= 1
+            right -= 1                              # 그렇지 않으면 가장 무거운 몸무게 인원만 태운다.
         answer += 1
     if left == right:
         answer += 1
