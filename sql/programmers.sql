314d0c9f0641f1a6f307f7230b9479b1db88cc83
PapyrusNotes <chris170841@gmail.com>
2022-02-05 22:22:01 +0900

    ADD sql problems

--- sql/programmers.sql
+++ sql/programmers.sql
@@ -3,9 +3,26 @@ SELECT *
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID ASC
 
-
 -- https://programmers.co.kr/learn/courses/30/lessons/59035
 SELECT NAME, DATETIME
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID DESC
+
+-- https://programmers.co.kr/learn/courses/30/lessons/59036
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+
+-- https://programmers.co.kr/learn/courses/30/lessons/59037
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION != 'Aged'
+
+-- https://programmers.co.kr/learn/courses/30/lessons/59403
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC
+
+
+
 
