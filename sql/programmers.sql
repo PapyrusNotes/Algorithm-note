c440a15079e85d65a6abd8698a5c8c36819c5b81
PapyrusNotes <chris170841@gmail.com>
2022-02-10 23:57:32 +0900

    SOLVE sql problems

--- sql/programmers.sql
+++ sql/programmers.sql
@@ -45,6 +45,9 @@ FROM ANIMAL_INS
 SELECT MIN(DATETIME)
 FROM ANIMAL_INS
 
+-- https://programmers.co.kr/learn/courses/30/lessons/59406
+SELECT COUNT(ANIMAL_ID) count
+FROM ANIMAL_INS
 
-
+-- https://programmers.co.kr/learn/courses/30/lessons/59408
 
