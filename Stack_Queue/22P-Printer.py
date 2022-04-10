467ae3e882d60895d1c32583e92e177c52613ecf
PapyrusNotes <chris170841@gmail.com>
2022-04-10 23:51:27 +0900

    REVISE 22P-printer

--- Stack_Queue/22P-Printer.py
+++ Stack_Queue/22P-Printer.py
@@ -5,24 +5,28 @@ from collections import deque
 
 def solution(priorities, location):
     q = deque()
+    flag = False
     age = 0
-    tgt = location
 
     for priority in priorities:
         q.append(priority)
 
-    while tgt >= 0:
-        flag = False
+    if not q:
+        return 0
+
+    while True:
         popped = q.popleft()
+        print('popped : ', popped)
+        location -= 1
+        print('location : ', location)
 
         for priority in q:
             if popped < priority:
-                q.append(popped)
                 flag = True
-                tgt -= 1
+                q.append(popped)
                 break
-        if flag:
-            continue
-        age += 1
 
-    return age
+        if not flag:
+            age += 1
+            if location < 0:
+                return age
