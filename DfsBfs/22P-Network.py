089e589c66484a2a82f1cca36bacf8a8fc208004
PapyrusNotes <chris170841@gmail.com>
2022-04-22 23:59:32 +0900

    REVISE 22P-Network

--- DfsBfs/22P-Network.py
+++ DfsBfs/22P-Network.py
@@ -1,5 +1,11 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/43162
 
-def pseudo_solution(n, computers):
+def solution(n, computers):
+    visited = []
+    stack = []
+
+    for computer in range(0, len(computers)):
+        visited.append(computer)
+        stack.append(computer)
     answer = 0
     return answer
