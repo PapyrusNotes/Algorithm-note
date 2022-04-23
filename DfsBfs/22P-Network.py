357ff5466b5713428abcb5d5f8ae5843b3c144c4
PapyrusNotes <chris170841@gmail.com>
2022-04-23 22:59:42 +0900

    REVISE 22P-Network

--- DfsBfs/22P-Network.py
+++ DfsBfs/22P-Network.py
@@ -1,11 +1,39 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/43162
 
-def solution(n, computers):
-    visited = []
+def travel_network(n, v, computers):
+    visited = [False] * n
     stack = []
 
-    for computer in range(0, len(computers)):
-        visited.append(computer)
-        stack.append(computer)
-    answer = 0
-    return answer
+    return visited
+
+
+def delete_visited(visited_node, computers):
+    for vn in visited_node:
+        del computers[vn]
+    return computers
+
+
+def solution(n, computers):
+    n_networks = 0
+
+    if n is 0:
+        return 0
+
+    # Start traveling networks from computer 0
+    while computers:
+        visited_node = travel_network(n, 0, computers)
+        computers = delete_visited(visited_node, computers)
+        n_networks += 1
+
+    return n_networks
+
+
+def main():
+    n = 3
+    computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+
+    print(solution(n, computers))
+
+
+if __name__ == '__main__':
+    main()
