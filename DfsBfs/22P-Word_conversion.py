27f443b9805f022a241e95779b74833d2f047247
PapyrusNotes <chris170841@gmail.com>
2022-05-02 23:36:50 +0900

    REVISE 22p-Word_conversion

--- DfsBfs/22P-Word_conversion.py
+++ DfsBfs/22P-Word_conversion.py
@@ -1,5 +1,16 @@
 # 출처 : https://programmers.co.kr/learn/courses/30/lessons/43162
 
+def is_replaceable(word1, word2) -> bool:
+    diff_n = 0
+    for i, character in enumerate(word1):
+        if character == word2[i]:
+            continue
+        else:
+            diff_n+=1
+            if diff_n == 2:
+                return False
+    return True
+
 def solution(begin, target, words):
     if target not in words:
         return 0
@@ -8,7 +19,7 @@ def solution(begin, target, words):
     level = 0
 
     while word != target:
-        pass
+
 
     return level
 
