9b5903c7f52e2e7ec5e9d2bd1b6929c54f159802
PapyrusNotes <chris170841@gmail.com>
2022-05-23 23:57:29 +0900

    REVISE 22P-flight tickets

--- DfsBfs/22P-Flight_tickets.py
+++ DfsBfs/22P-Flight_tickets.py
@@ -2,19 +2,41 @@
 
 
 def solution(tickets):
+    stack = []
     answer = []
-    departure = []
+    visited = [False]*len(tickets)
+    candidates = []
 
     for ticket in tickets:
         if ticket[0] == 'ICN':
-            departure.append(ticket)
+            candidates.append(ticket)
+
+    departure = candidates[0]
+
+    for i in range(0, len(candidates) - 1):
+        if candidates[i][1] < candidates[i + 1][1]:
+            departure = i
+
+    stack.append(departure)
+    visited[departure] = True
+    while stack:
+        top = stack[-1]
+        arrival = tickets[top][1]
+        for i, ticket in enumerate(tickets):
+            if visited[i]:
+                continue
+            elif arrival == ticket[0]:
+                stack.append(i)
+                visited[i] = True
+                break
+
+
 
 
     return answer
 
 
 if __name__ == '__main__':
-
     tickets1 = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
     tickets2 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
 
