dfd1ddd7157d5b4a080d525913e4de3d463bed4e
PapyrusNotes <chris170841@gmail.com>
2022-06-02 23:57:50 +0900

    ADD 22P-Joystick

---
+++ greedy/22P-Joystick.py
@@ -0,0 +1,18 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42860
+
+def solution(name):
+    answer = 0
+    return answer
+
+
+def main():
+    name1 = "JEROEN"
+    name2 = "JAN"
+
+    print(solution(name1))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ greedy/22P-Stolen_uniform.py
@@ -0,0 +1,21 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42862
+
+def solution(n, lost, reserve):
+    
+
+    answer = 0
+    return answer
+
+
+def main():
+    n = 5
+    lost = [2, 4]
+    reserve = [1, 3, 5]
+
+    print(solution(n, lost, reserve))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
