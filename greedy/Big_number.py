ae6a62c6bf17c30cdf91fe12d8325d4ec494c8c7
PapyrusNotes <chris170841@gmail.com>
2022-06-06 22:03:44 +0900

    SOLVED Big_number

--- greedy/Big_number.py
+++ greedy/Big_number.py
@@ -1,20 +1,29 @@
 # 출처 : 2019 국가 교육기관 코딩 테스트, 큰 수의 법칙
 
 def solution(n, m, k, data):
-    data = data.sort()
+    data.sort()
     max_num = data[n-1]
     next_max_num = data[n-2]
 
     big_num = 0
+    i = 1
+
+    while i <= m:
+        if i % k != 0:
+            big_num += max_num
+        else:
+            big_num += next_max_num
+        i += 1
+
+    return big_num
 
 
 def main():
 
     n, m, k = map(int, input().split())
     data = list(map(int, input().split()))
-    print('before n : ', n)
     print('Biggest number is : ', solution(n, m, k, data))
-    print('after n : ', n)
+
     return 0
 
 
