6ff06290ff1b612238384090a9cb15a29b5d1e2a
PapyrusNotes <chris170841@gmail.com>
2022-06-12 23:42:44 +0900

    ADD 22P-Stolen_uniform_ver2

---
+++ greedy/22P-Stolen_uniform_ver2.py
@@ -0,0 +1,19 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42862
+# Using two pointer
+def solution(n, lost, reserve):
+    answer = 0
+    return answer
+
+
+def main():
+    n = 7
+    lost = [2, 3, 4]
+    reserve = [1, 2, 3, 6]
+
+    print(solution(n, lost, reserve))
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
