47a35b4dee9f7adf1747740022d392cb28f4978f
PapyrusNotes <chris170841@gmail.com>
2022-06-13 23:59:20 +0900

    ADD 22P-Big_number

---
+++ greedy/22P-Big_number.py
@@ -0,0 +1,17 @@
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42883
+
+# 출처 : https://programmers.co.kr/learn/courses/30/lessons/42862
+# Using two pointer
+def solution(n, lost, reserve):
+    answer = ''
+    return answer
+
+
+def main():
+
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
