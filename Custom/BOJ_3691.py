8cf887829001a78950330598a5619bcb0d1ba166
PapyrusNotes <chris170841@gmail.com>
2025-03-12 12:23:01 +0900

    <add>: add restored coderbyte test

---
+++ Custom/BOJ_3691.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+test_case = int(input())
+
+@dataclass
+class PartEstimate:
+
+
+def get_input():
+
+def solution(n_part: int, budget: int, part_info: PartEstimate) -> int:
+    return best_quality
+
+for i in range(test_case):
+    n_part, budget = map(int, input().split())
+    part_info = get_input()
+    print(solution(n_part,budget,part_info))
